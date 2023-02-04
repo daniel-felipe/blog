3c0eb2cf17338e5bb1629e81e1690776aeef7a8c
Daniel Felipe <daniell.felipe.2017@gmail.com>
2023-02-04 09:17:21 -0300

    Creating Comment model and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             CategorySeeder::class,
             TagSeeder::class,
-            PostSeeder::class,
             PermissionUserSeeder::class,
+            CommentSeeder::class,
         ]);
     }
 }
