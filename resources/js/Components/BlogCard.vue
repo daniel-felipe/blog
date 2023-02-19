bcd8c5ed4e1295a7badd718a45d81076c9b7df0d
Daniel Felipe <daniell.felipe.2017@gmail.com>
2023-02-19 12:04:07 -0300

    Finishing card component

--- resources/js/Components/BlogCard.vue
+++ resources/js/Components/BlogCard.vue
@@ -5,21 +5,29 @@ defineProps({
 </script>
 
 <template>
-    <div v-for="post in posts" :key="post.id" class="w-80 bg-white min-h-fit rounded-md border dark:bg-gray-800 dark:text-white dark:border dark:border-gray-700">
-        <div class="grid grid-rows-2">
-            <div class="bg-blue-200 h-48 rounded-t-md"></div>
+    <div v-for="post in posts" :key="post.id" class="bg-white border rounded-md w-80 h-fit dark:bg-gray-800 dark:text-white dark:border dark:border-gray-700">
+        <div class="flex flex-col">
+            <div class="h-4/12 g-blue-100 rounded-t-md">
+                <img src="https://via.placeholder.com/640x370" alt="thumbnail">
+            </div>
 
-            <div class="p-4 flex flex-col justify-between gap-4">
-                <div class="space-x-3">
-                    <span class="px-2 rounded-md bg-gray-200 text-gray-600 font-bold">{{ post.category.name }}</span>
-                    <span class="text-xs text-gray-500 dark:text-gray-400">{{ post.published_at }}</span>
+            <div class="flex flex-col items-stretch gap-4 p-4 h-8/12">
+                <div class="flex items-center justify-start space-x-3">
+                    <div class="space-x-3">
+                        <span class="px-2 font-bold text-gray-600 bg-gray-200 rounded-md">{{ post.category.name }}</span>
+                        <span class="text-xs text-gray-500 dark:text-gray-400">{{ post.published_at }}</span>
+                    </div>
                 </div>
 
-                <h2 class="text-2xl font-bold">{{ post.title }}</h2>
+                <div class="py-6 min-h-fit">
+                    <h2 class="text-2xl font-bold">{{ post.title.substr(1, 32) }}</h2>    
+                </div>
 
-                <p class="text-gray-600">
-                    {{ post.description }}
-                </p>
+                <div class="border-l-4 border-gray-300">
+                    <p class="ml-3 text-gray-500">
+                        {{ post.author.name }}
+                    </p>
+                </div>
             </div>
         </div>
     </div>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -6,6 +6,7 @@ import BlogCard from '@/Components/BlogCard.vue';
 
 defineProps({
     posts: Object,
+    categories: Object,
 });
 
 </script>
@@ -14,8 +15,23 @@ defineProps({
         <BlogContainer class="py-24">
             <Head title="Home Page" />
 
-            <div class="grid grid-cols-3 gap-12">
-                <BlogCard :posts="posts" />
+            <div class="flex gap-3">
+                <aside class="w-2/12 space-y-2 min-w-fit">
+                    <h2 class="pl-3 font-semibold tracking-widest text-gray-400 uppercase">categorias</h2>
+
+                    <p v-for="category in categories" :key="category.id" class="flex items-center gap-3 px-3 py-2 text-gray-600 transition rounded cursor-pointer hover:bg-gray-200 hover:text-blue-300 dark:text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+
+
+                        {{ category.name }}
+                    </p>
+                </aside>
+
+                <div class="grid items-stretch w-10/12 grid-cols-3 gap-12">
+                    <BlogCard :posts="posts.data" />
+                </div>
             </div>
         </BlogContainer>
     </BlogLayout>
